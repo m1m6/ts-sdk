7c0a3f43dd8ec26bbc428e244368858738727be3
mahmoudjbour <mjbour@kiitos-tech.com>
2020-12-03 19:04:11 +0200

    ad

--- sdk.js
+++ sdk.js
@@ -556,6 +556,7 @@ const TS_STACK_SELECTED_LANG = 'ts-stack-sl';
                                             window.translatedStringsMap[globalIndex] &&
                                             !window.translatedStringsMap[globalIndex].isReplaced
                                         ) {
+                                            console.log("node.name", node.name);
                                             if (node.name === 'description') {
                                                 node.content = node.content.replace(from, to);
                                             } else {
